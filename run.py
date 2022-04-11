753afadf9b2ba66b1d2de08e8a5e4934b795651a
Sarah Shaheen <sarahshaheen1@hotmail.com>
2022-04-11 11:06:53 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Tic-Tac-Toe
+
+
+import random
+
+
+def selectPlayerLetter():
+    # Let's the player pick what letter they want to be.
+    # Returns a list with the players letter as the first item and
+    # the computers letter as the second item.
+
+    letter = " "
+    while not (letter == 'X' or letter == 'O'):
+        print("Do you want to be X or O?")
+        letter = input().upper()
+
+    if letter == 'X':
+        return ["X", "O"]
+    else:
+        return ["O", "X"]
+
